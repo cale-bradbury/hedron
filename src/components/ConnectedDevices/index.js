4ef4d4f7df6402d4cd29d72ca807001f08831922
cale bradbury <cale.bradbury@gmail.com>
2024-04-05 19:52:15 -0400

    display audio device dropdown on main screen

--- src/components/ConnectedDevices/index.js
+++ src/components/ConnectedDevices/index.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import Device from '../../containers/Device'
+import styled from 'styled-components'
 import PropTypes from 'prop-types'
+import Input from '../Input'
+import Select from '../../containers/Select'
 
-const ConnectedDevices = ({ items }) => (
+
+const Wrapper = styled.div`
+  padding: 0.5rem;
+  border: 1px solid #111;
+  margin-bottom: 0.5rem;
+  display: flex;
+  height: 2.5rem;
+  align-items: center;
+`
+
+const ConnectedDevices = ({ items, audio }) => (
   <div>
-    <h6>Connected Devices</h6>
+    <h6>Connected Audio Devices</h6>
+    <ul>
+      <Wrapper>
+        <form onSubmit={e => e.preventDefault()}>
+          <Input
+            name='audioChannel'
+            id={`audioChannel`}
+            label='Audio Input Channel'
+            component={Select}
+            layout='compact'
+            options={
+              audio.map((item, index) => ({ value: item.id, label: item.title }))
+            }
+          />
+        </form>
+      </Wrapper>
+    </ul>
+    <h6>Connected MIDI Devices</h6>
     <ul>
       {items.map(item => {
         return (
@@ -21,6 +51,10 @@ ConnectedDevices.propTypes = {
   items: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.string.isRequired,
   })),
+  audio: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+  }))
 }
 
 export default ConnectedDevices
--- src/containers/ConnectedDevices/index.js
+++ src/containers/ConnectedDevices/index.js
@@ -1,16 +1,29 @@
 import { connect } from 'react-redux'
 import ConnectedDevices from '../../components/ConnectedDevices'
 import getConnectedDevices from '../../selectors/getConnectedDevices'
+import getConnectedAudioInputs from '../../selectors/getConnectedAudioInputs'
+import { audioSettingsUpdate } from '../../store/audio/actions'
+import { reduxForm } from 'redux-form'
 
 const mapStateToProps = (state, ownProps) => ({
   items: getConnectedDevices(state),
+  audio: getConnectedAudioInputs(state)
 })
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onChange: (values) => {
+    console.error('values', values)
+    dispatch(audioSettingsUpdate(ownProps.audio[0].id))
+  },
+})
+
+const Device = reduxForm()(ConnectedDevices)
 
 export default connect(
   mapStateToProps,
-  null,
+  mapDispatchToProps,
   null,
   {
     areStatesEqual: (next, prev) => next.midi === prev.midi,
   }
-)(ConnectedDevices)
+)(Device)
